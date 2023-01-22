9f5759292cbd9f11fbcac6605a00d1d4ff46ec03
fereshtehbaradaran <bara.frsht@gmail.com>
2023-01-22 16:34:16 +0330

    input and output pins added

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,8 +4,29 @@
 #include <SD.h>
 #include <Arduino_FreeRTOS.h>
 
+#define MOTOR_PORT 6
+#define LED_PORT 5
+
+#define LM35_PORT A2
+#define PHOTOCELL_1 A0
+#define PHOTOCELL_2 A1
+#define DIP_SW_1 2
+#define DIP_SW_2 3
+
+#define UART_DELAY 20000   //2 seconds
+#define OFF_TEMPERATURE 25
+
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(MOTOR_PORT,OUTPUT);
+  pinMode(LED_PORT, OUTPUT);
+
+  pinMode(LM35_PORT, INPUT);
+  pinMode(PHOTOCELL_1, INPUT);
+  pinMode(PHOTOCELL_2, INPUT);
+  pinMode(DIP_SW_1, INPUT);
+  pinMode(DIP_SW_2, INPUT);
+  
 }
 
 void loop() {
