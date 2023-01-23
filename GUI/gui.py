224f4a9c10f6b0dc47d58ad1bb0c4b4ab33f2c29
helia <heliyash4380>
2023-01-23 13:04:54 +0330

    labels changed

--- GUI/gui.py
+++ GUI/gui.py
@@ -8,15 +8,19 @@ import tkinter as tk
 import serial
 import serial.tools.list_ports
 import time
+import uart
 
 
 def showPower():  
    sel = "Power = " + str(CoolerValue.get())  
-   CoolerLabel.config(text = sel)  
+   CoolerLabel.config(text = sel)
+   return CoolerValue.get()
 
 def showLight():  
    sel = "Light = " + str(lightValue.get())  
-   lightLabel.config(text = sel)  
+   lightLabel.config(text = sel)
+   return lightValue.get()
+
 
 root = tk.Tk()
 root.config(background = atk.DEFAULT_COLOR)
@@ -30,18 +34,18 @@ s.theme_use("default")
 f1 = atk.Frame3d(root)
 f1.pack(side = 'left', expand = True, fill = 'both', padx = 3, pady = 3)
 
-temp = tk.Label(f1, text = "Cooler", fg = "black", bg = 'purple')
+temp = tk.Label(f1, text = "Cooler", fg = "black", bg = '#0dbee1', font=("Helvetica", 14))
 temp.pack(fill = 'x', padx = 5)
 
 currentCoolerValue = 20
-CoolerProgress = atk.RadialProgressbar3d(f1, fg = 'purple', size = 170)
+CoolerProgress = atk.RadialProgressbar3d(f1, fg = '#0dbee1', size = 170)
 CoolerProgress.pack(padx = 20, pady = 20)
 CoolerProgress.set(currentCoolerValue)
 CoolerProgress.stop()
 
 CoolerValue = DoubleVar()  
 CoolerScale = Scale(f1, variable = CoolerValue, from_ = 0, to = 100, orient = HORIZONTAL, length = 300, showvalue = 1)
-CoolerScale.pack(padx = 15, anchor = CENTER)  
+CoolerScale.pack(anchor = CENTER, padx = 15)  
 CoolerScale.set(currentCoolerValue)
   
 CoolerBtn = atk.Button3d(f1, text = "Set", command = showPower)
@@ -55,18 +59,18 @@ CoolerLabel.pack(pady = 5)
 f2 = atk.Frame3d(root)
 f2.pack(side = 'left', expand = True, fill = 'both', padx = 3, pady = 3)
 
-temp = tk.Label(f2, text = "Light", bg = 'yellow', fg = "black")
+temp = tk.Label(f2, text = "Light", bg = '#f2e05b', fg = "black")
 temp.pack(fill = 'x', padx = 5)
 
 currentLightValue = 40
-lightProgress = atk.RadialProgressbar3d(f2, fg = 'yellow', size = 170)
+lightProgress = atk.RadialProgressbar3d(f2, fg = '#f2e05b', size = 170)
 lightProgress.pack(padx = 20, pady = 20)
 lightProgress.set(currentLightValue)
 lightProgress.stop()
 
 lightValue = DoubleVar() 
 light = Scale(f2, variable = lightValue, from_ = 0, to = 100, orient = HORIZONTAL, length= 300, showvalue = 1)
-light.pack(anchor = CENTER)  
+light.pack(anchor = CENTER, pady = 10)  
 light.set(currentLightValue)
 
 lightBtn = atk.Button3d(f2, text = "Set", command = showLight)
@@ -76,7 +80,3 @@ lightLabel = Label(f2)
 lightLabel.pack(pady = 5) 
 
 root.mainloop()
-
-
-
-
